de133e5ad69ea57b857b6181c22abc65e24eccea
mohammad hossein aghajani <147534501+mohammadhsagh@users.noreply.github.com>
2023-10-10 19:34:41 +0200

    Delete insert.sql

--- insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `users`(`username`, `firstname`, `lastname`, `password`) VALUES ('[ahmad123]','[ahmad]','[gholami]','[ahm@ad13]')
-
-INSERT INTO `message`(`message text`) VALUES ('[hello world]')
