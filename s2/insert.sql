0c61d98cc784b074a16e4063df96af63fa8434da
mohammad hossein aghajani <147534501+mohammadhsagh@users.noreply.github.com>
2023-10-10 19:38:39 +0200

    Update insert.sql

--- s2/insert.sql
+++ s2/insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO `users`(`username`, `firstname`, `lastname`, `password`) VALUES ('[ahmad123]','[ahmad]','[gholami]','[ahm@ad13]')
+INSERT INTO `users`(`id`,`username`, `firstname`, `lastname`, `password`) VALUES ('1','ahmad123','ahmad','gholami','ahm@ad13')
 
-INSERT INTO `message`(`message text`) VALUES ('[hello world]')
+INSERT INTO `message`(`message text`) VALUES ('hello world')
