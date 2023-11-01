5f4dcf475d37f6928a2818ea4e2385c6fbbb8ebb
mohammad hossein aghajani <147534501+mohammadhsagh@users.noreply.github.com>
2023-11-01 19:10:28 +0330

    Add files via upload

---
+++ s5/selectallusers.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "usermessage";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM user";
+
+$result = $dbc -> query( $sql );
+$table = $result -> fetch_all( MYSQL_ASSOC );
+
+
+$conn->close();
+?> 
